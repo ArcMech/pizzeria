aaa99b971fb869796719d588b19bb155611fc90a
ArcMech <arek.krz@gmail.com>
2019-06-13 14:52:21 +0200

    changed order of section, added smooth scrolling after clicking anchor link

--- src/js/main/order.js
+++ src/js/main/order.js
@@ -267,3 +267,12 @@ document.querySelector(".dropdownBackground").addEventListener("click", e => {
     return 0;
   }
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
